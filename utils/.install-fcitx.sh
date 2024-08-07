293dc11674ee09d441a624a22d948dbac71f903b
Sizhe Liu <sizhe.liu@fit-foxconn.com>
2024-08-07 17:09:00 -0500

    add fcitx install shell

---
+++ utils/.install-fcitx.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+# stow fcitx if fcitx4 is used
+# kitty does not support chn input using fcitx
+# sudo apt install fcitx fcitx-bin fcitx-config-gtk fcitx-googlepinyin im-config
+# install fcitx5
+sudo apt install fcitx5 fcitx5-chinese-addon im-config
+# run im-config afterwards to choose fcitx(5) as default method
+
