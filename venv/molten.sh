aa30b052d223451afef73a55217c91c6aca18a7c
Sizhe Liu <sizhe.liu@fit-foxconn.com>
2024-09-02 12:14:32 -0500

    update venv/molten.sh

--- venv/molten.sh
+++ venv/molten.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
+# uncomment the following line if you don't have the folder
 # mkdir ~/.virtualenvs
+sudo apt-get install luajit
+sudo apt-get install libmagickwand-dev
+sudo apt-get install luarocks
+luarocks install magick
 python3 -m venv ~/.virtualenvs/molten # create a new venv
 source ~/.virtualenvs/molten/bin/activate # activate the venv
 pip install pynvim jupyter_client cairosvg plotly kaleido pnglatex pyperclip matplotlib numpy pandas scikit-learn
