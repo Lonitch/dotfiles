e73817d6d8aae897507d8324370b565264e5e51e
Sizhe Liu <sizhe.liu@fit-foxconn.com>
2024-09-09 22:25:05 -0500

    add typst-lsp

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,73 +1,75 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				-- define LSP servers here for LUA, RUST, JS/TS, and PYTHON
-				ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", "jedi_language_server" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lsp_disabled = vim.g.lsp_disabled or false
-			if lsp_disabled then
-				return
-			end
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-			-- Capabilities required for the visualstudio lsps (css, html, etc)
-			capabilities.textDocument.completion.completionItem.snippetSupport = true
-			local lspconfig = require("lspconfig")
-      lspconfig.jedi_language_server.setup({
-				capabilities = capabilities,
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        -- define LSP servers here for LUA, RUST, JS/TS, and PYTHON
+        ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", "jedi_language_server", "typst_lsp" },
       })
-			-- lspconfig.pyright.setup({
-			-- 	capabilities = capabilities,
-      --  cmd = { "bun", "run", "pyright-langserver", "--stdio" },
-			-- })
-			-- lspconfig.tailwindcss.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
-			-- lspconfig.html.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-				cmd = { "bun", "run", "typescript-language-server", "--stdio" },
-			})
-			-- lspconfig.rust_analyzer.setup({
-			-- 	capabilities = capabilities,
-			-- 	filetypes = { "rs" },
-			-- 	settings = {
-			-- 		["rust-analyzer"] = {
-			-- 			cargo = { allFeatures = true },
-			-- 			procMacro = {
-			-- 				ignored = {
-			-- 					leptos_macro = {
-			-- 						-- optional: --
-			-- 						"component",
-			-- 						"server",
-			-- 					},
-			-- 				},
-			-- 			},
-			-- 		},
-			-- 	},
-			-- })
-		end,
-	},
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lsp_disabled = vim.g.lsp_disabled or false
+      if lsp_disabled then
+        return
+      end
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+      -- Capabilities required for the visualstudio lsps (css, html, etc)
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+      local lspconfig = require("lspconfig")
+      lspconfig.jedi_language_server.setup({
+        capabilities = capabilities,
+      })
+      require("lspconfig").typst_lsp.setup({
+        settings = {
+          exportPdf = "never", -- Choose onType, onSave or never.
+          -- serverPath = "" -- Normally, there is no need to uncomment it.
+        },
+      })
+      -- lspconfig.tailwindcss.setup({
+      -- 	capabilities = capabilities,
+      -- })
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
+      -- lspconfig.html.setup({
+      -- 	capabilities = capabilities,
+      -- })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+        cmd = { "bun", "run", "typescript-language-server", "--stdio" },
+      })
+      -- lspconfig.rust_analyzer.setup({
+      -- 	capabilities = capabilities,
+      -- 	filetypes = { "rs" },
+      -- 	settings = {
+      -- 		["rust-analyzer"] = {
+      -- 			cargo = { allFeatures = true },
+      -- 			procMacro = {
+      -- 				ignored = {
+      -- 					leptos_macro = {
+      -- 						-- optional: --
+      -- 						"component",
+      -- 						"server",
+      -- 					},
+      -- 				},
+      -- 			},
+      -- 		},
+      -- 	},
+      -- })
+    end,
+  },
 }
