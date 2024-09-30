f052e2755184bc8c74eeb403700335c7c18dceb7
Sizhe Liu <sizhe.liu@fit-foxconn.com>
2024-09-30 18:40:14 -0500

    utils: delete switch-keyboard-input

--- utils/.switch-keyboard-input.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-# use this script if you prefer ibus
-current_engine=$(ibus engine)
-
-if [ "$current_engine" = "xkb:us::eng" ]; then
-    ibus engine libpinyin
-else
-    ibus engine xkb:us::eng
-fi
